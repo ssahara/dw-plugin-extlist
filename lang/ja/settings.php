73aae6f185287abbb8660cea76ae3aaa5e97af2c
Satoshi Sahara <sahara.satoshi@gmail.com>
2016-07-23 11:19:30 +0900

    fix settings text and update plugin.info

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -4,7 +4,7 @@
  * @author     Satoshi Sahara <sahara.satoshi@gmail.com>
  */
 
-$lang['use_plugin_css'] = 'sample.less を all.less として使用する（上書きコピー）';
+$lang['use_plugin_css'] = 'sample.less を all.less として使用する（存在しない場合、コピーする）';
 
 $lang['dl_class'] = '説明リスト用のデフォルトクラス';
 $lang['ol_class'] = '順序付きリスト用のデフォルトクラス';
